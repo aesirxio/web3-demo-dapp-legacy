8fb932a400a7aa0d102ffbdbbc6187fd8eeb78e9
Tito Alvarez <augustoalvarez@gmail.com>
2023-02-05 16:06:02 -0600

    Concordium includes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
