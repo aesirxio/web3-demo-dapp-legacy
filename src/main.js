983d6c37a3761cb33f0e839400d8c0c13c6d88d9
Tito Alvarez <augustoalvarez@gmail.com>
2023-02-07 23:45:52 -0600

    dApp with connection and NFT mint

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "sweetalert2/dist/sweetalert2.min.css";
 import "./assets/main.css";
 
 const app = createApp(App);
@@ -12,3 +13,5 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+console.log(import.meta.env);
